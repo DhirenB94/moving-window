388040a8dd4d2d441d84819cef5699ea332e11ae
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2023-10-09 19:02:34 +0100

    setup up the router in the NewRequestsServer

--- server.go
+++ server.go
@@ -14,15 +14,22 @@ type RequestsStore interface {
 
 type RequestsServer struct {
 	reqStore RequestsStore
+	http.Handler
 }
 
 func NewRequestServer(reqStore RequestsStore) *RequestsServer {
-	return &RequestsServer{
-		reqStore: reqStore,
-	}
+	rs := new(RequestsServer)
+	rs.reqStore = reqStore
+
+	router := http.NewServeMux()
+	router.Handle("/", http.HandlerFunc(rs.reqCountsHandler))
+	rs.Handler = router
+
+	return rs
+
 }
 
-func (rs *RequestsServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (rs *RequestsServer) reqCountsHandler(w http.ResponseWriter, r *http.Request) {
 	currentTime := rs.reqStore.GetCurrentTime()
 	reqsInLastMin := rs.reqStore.GetReqsInLastMin(currentTime)
 	fmt.Fprint(w, reqsInLastMin)
