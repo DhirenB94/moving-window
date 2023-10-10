d3dd0446331ba8b6c84ecd7e8ab916e5c2392f77
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2023-10-10 09:36:27 +0100

    Working FileSystem Solution

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,15 +3,24 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	movingwindow "dhiren.brahmbhatt/moving-window"
 )
 
+const fileName = "count.db.json"
+
 func main() {
-	store := movingwindow.NewInMemDB()
-	server := movingwindow.NewRequestServer(store)
-	err := http.ListenAndServe(":5001", server)
+	file, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE, 0666)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("unable to open file %s, %v", fileName, err)
+	}
+
+	store := movingwindow.NewFileSystem(file)
+	server := movingwindow.NewRequestServer(store)
+
+	err = http.ListenAndServe(":5001", server)
+	if err != nil {
+		log.Fatalf("could not listen on port 5001 %v", err)
 	}
 }
