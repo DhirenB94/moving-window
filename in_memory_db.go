c7e27557c5092a14bc92a626a793f3f1fab7be2c
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2023-10-10 09:59:06 +0100

    comment out InMemDB solution

--- in_memory_db.go
+++ in_memory_db.go
@@ -1,49 +1,49 @@
 package movingwindow
 
-import (
-	"sync"
-	"time"
-)
+// import (
+// 	"sync"
+// 	"time"
+// )
 
-type InMemDB struct {
-	reqSecondCount map[int]int
-	mutex          sync.Mutex
-}
+// type InMemDB struct {
+// 	reqSecondCount map[int]int
+// 	mutex          sync.Mutex
+// }
 
-func NewInMemDB() *InMemDB {
-	return &InMemDB{
-		reqSecondCount: map[int]int{
-			0: 0,
-		},
-		mutex: sync.Mutex{},
-	}
-}
+// func NewInMemDB() *InMemDB {
+// 	return &InMemDB{
+// 		reqSecondCount: map[int]int{
+// 			0: 0,
+// 		},
+// 		mutex: sync.Mutex{},
+// 	}
+// }
 
-func (im *InMemDB) GetReqsInLastMin(reqSecond int) int {
-	im.mutex.Lock()
-	defer im.mutex.Unlock()
+// func (im *InMemDB) GetReqsInLastMin(reqSecond int) int {
+// 	im.mutex.Lock()
+// 	defer im.mutex.Unlock()
 
-	requestsInLastMin := 0
-	// get the begining of the last minute
-	oneMinAgo := reqSecond - 60
+// 	requestsInLastMin := 0
+// 	// get the begining of the last minute
+// 	oneMinAgo := reqSecond - 60
 
-	// Iterate through the requestCount map and if the time is after the last minute starts, sum those values
-	for second, count := range im.reqSecondCount {
-		if second >= oneMinAgo {
-			requestsInLastMin += count
-		}
-	}
+// 	// Iterate through the requestCount map and if the time is after the last minute starts, sum those values
+// 	for second, count := range im.reqSecondCount {
+// 		if second >= oneMinAgo {
+// 			requestsInLastMin += count
+// 		}
+// 	}
 
-	return requestsInLastMin
-}
+// 	return requestsInLastMin
+// }
 
-func (im *InMemDB) AddReqToCount(reqSecond int) {
-	//Update the requestCount map for the current second
-	im.mutex.Lock()
-	defer im.mutex.Unlock()
-	im.reqSecondCount[reqSecond]++
-}
+// func (im *InMemDB) AddReqToCount(reqSecond int) {
+// 	//Update the requestCount map for the current second
+// 	im.mutex.Lock()
+// 	defer im.mutex.Unlock()
+// 	im.reqSecondCount[reqSecond]++
+// }
 
-func (im *InMemDB) GetCurrentSecond() int {
-	return int(time.Now().Unix())
-}
+// func (im *InMemDB) GetCurrentSecond() int {
+// 	return int(time.Now().Unix())
+// }
